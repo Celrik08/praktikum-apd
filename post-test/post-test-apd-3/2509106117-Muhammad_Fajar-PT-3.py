644e0885dc6ede1516c20977686e37244eea3550
Celrik08 <m.fajar080907@gmail.com>
2025-10-01 22:15:19 +0800

    Memperbaiki File Python, dan File PDF

--- post-test/post-test-apd-3/2509106117-Muhammad_Fajar-PT-3.py
+++ post-test/post-test-apd-3/2509106117-Muhammad_Fajar-PT-3.py
@@ -1,16 +1,11 @@
 print("=== Penghitung Gaji Karyawan PT. BOM ===")
 nama = input("Masukkan nama karyawan: ")
 jabatan = input("Masukkan jabatan karyawan (peracik/pengantar): ").lower()
-hari_kerja = int(input("Masukkan jumlah hari kerja: "))
-jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
-lembur = int(input("Masukkan jumlah lembur: "))
-
-harga_petasan = 5000
-
-bayaran_per_jam = 0
-bayaran_per_lembur = 0
 
 if jabatan == "peracik":
+    hari_kerja = int(input("Masukkan jumlah hari kerja: "))
+    jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
+    lembur = int(input("Masukkan jumlah lembur: "))
     if hari_kerja >= 24 and jam_kerja >= 8 and lembur >= 4:
         bayaran_perjam = 25000
         bayaran_lembur = 15000
@@ -22,6 +17,9 @@ if jabatan == "peracik":
         bayaran_lembur = 10000
 
 elif jabatan == "pengantar":
+    hari_kerja = int(input("Masukkan jumlah hari kerja: "))
+    jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
+    lembur = int(input("Masukkan jumlah lembur: "))
     if hari_kerja >= 20 and jam_kerja >= 7 and lembur >= 7:
         bayaran_perjam = 25000
         bayaran_lembur = 20000
@@ -40,7 +38,6 @@ total_gaji = ((bayaran_perjam * jam_kerja) * hari_kerja) + (lembur * bayaran_lem
 print("\n=== Hasil Perhitungan Gaji ===")
 print(f"Nama Karyawan     : {nama}")
 print(f"Jabatan           : {jabatan.capitalize()}")
-print("Harga Petasan   : Rp", f"{harga_petasan:,}")
 print(f"Hari Kerja        : {hari_kerja} hari")
 print(f"Jam Kerja         : {jam_kerja} jam/hari")
 print(f"Jumlah Lembur     : {lembur} kali")
