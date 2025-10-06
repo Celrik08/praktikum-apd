c597610793000730ef4ad2022feec96c9d72eb6a
Celrik08 <m.fajar080907@gmail.com>
2025-10-06 09:11:20 +0800

    Memperbaiki File Python di Folder Kelas

--- kelas/pertemuan-2/pertemuan-2.py
+++ kelas/pertemuan-2/pertemuan-2.py
@@ -1,1 +1,50 @@
-print("Hello World")
+# a = 10
+# b = 3
+# c = a + b
+# print(c)
+
+# a = 0,13
+# b = 0,15
+# c = a+b
+# print(c)
+
+# Nama = 'Sifwah'
+# Hobi = "ngoding"
+# print(Nama)
+# print(Hobi)
+
+# print ("""Halo,
+# Saya sedang belajar ngoding
+# Bahasa Pemrograman python""")
+
+# print("halo""saya""daffa")
+# print("halo"+"daffa")
+
+# teks = 'saya suka belajar apd'
+# #menampilkan index ke-3
+# print(teks[3])
+# #menampilkan index ke-0 hingga ke-3
+# print(teks[0:3])
+# #menampilkan seluruhnya, tetapi dimulai dari index ke-5
+# print(teks[5:])
+# #menampilkan hanya sampai index ke-8
+# print(teks[:8])
+
+# Hujan = False
+# Panas = True
+
+# prodi = ['Informatika', 'Sistem Informasi', 'Teknik Sipil']
+# print (prodi[0])
+# print(prodi[1])
+# print(prodi[2])
+
+# warna = {
+# 'warna1' : 'merah',
+# 'warna2' : 'kuning',
+# 'warna3' : 'hijau'
+# }
+# print(warna['warna1'])
+# print(warna['warna3'])
+
+# nama = input('masukkan Nama Anda : ')
+# print(nama)
